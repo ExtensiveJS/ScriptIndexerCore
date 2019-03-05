2f6cc25847133f2f74e8588f3a89a1e345ca23ef
BrentPresley <brent.presley@mstc.edu>
2019-03-05 06:58:53 -0600

    list view.  Partial detail controller and view. WIP.

--- ScriptIndexerCore/Controllers/HomeController.cs
+++ ScriptIndexerCore/Controllers/HomeController.cs
@@ -4,15 +4,175 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using ScriptIndexerCore.Models;
 
 namespace ScriptIndexerCore.Controllers
 {
     public class HomeController : Controller
     {
+        private IMongoDatabase mongoDatabase;
+
+        //Generic method to get the mongodb database details  
+        public IMongoDatabase GetMongoDatabase()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            return mongoClient.GetDatabase("scripts");
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            //Get the database connection  
+            mongoDatabase = GetMongoDatabase();
+
+            //fetch the details from CustomerDB and pass into view  
+            var result = mongoDatabase.GetCollection<Movie>("moviescripts").Find(FilterDefinition<Movie>.Empty).ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            try
+            {
+                //Get the database connection  
+                mongoDatabase = GetMongoDatabase();
+                mongoDatabase.GetCollection<Movie>("moviescripts").InsertOne(movie);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(string movieID)
+        {
+            string currMovieId = RouteData.Values["id"].ToString();
+            if (currMovieId == null)
+            {
+                return NotFound();
+            }
+            //Get the database connection  
+            mongoDatabase = GetMongoDatabase();
+
+            //fetch the details from DB and pass into view 
+            var collection = mongoDatabase.GetCollection<Movie>("moviescripts");
+
+            //obtain objectid with our guid
+            var filter_id = Builders<Movie>.Filter.Eq("id", ObjectId.Parse(currMovieId));
+            //not finding movie
+            Movie movie = collection.Find<Movie>(filter_id).FirstOrDefault();
+
+            //var builder = Builders<Movie>.Filter;
+            //var filter = builder.Eq(x => x.Id.ToString(), currMovieId);
+            //var movie = collection.Find(filter).FirstOrDefault();
+
+
+
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //Get the database connection  
+            mongoDatabase = GetMongoDatabase();
+            //fetch the details from CustomerDB and pass into view  
+            string idStr = id.ToString();
+            Movie movie = mongoDatabase.GetCollection<Movie>("moviescripts").Find<Movie>(k => k.Id.ToString() == idStr).FirstOrDefault();
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            try
+            {
+                //Get the database connection  
+                mongoDatabase = GetMongoDatabase();
+                //Delete the customer record  
+                var result = mongoDatabase.GetCollection<Movie>("moviescripts").DeleteOne<Movie>(k => k.Id.ToString() == movie.Id.ToString());
+                if (result.IsAcknowledged == false)
+                {
+                    return BadRequest("Unable to Delete Movie " + movie.Id.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //Get the database connection  
+            mongoDatabase = GetMongoDatabase();
+            //fetch the details from CustomerDB based on id and pass into view  
+            string idStr = id.ToString();
+            var movie = mongoDatabase.GetCollection<Movie>("moviescripts").Find<Movie>(k => k.Id.ToString() == idStr).FirstOrDefault();
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            try
+            {
+                //Get the database connection  
+                mongoDatabase = GetMongoDatabase();
+                //Build the where condition  
+                var filter = Builders<Movie>.Filter.Eq("_Id", movie.Id.ToString());
+                //Build the update statement   
+                var updatestatement = Builders<Movie>.Update.Set("_Id", movie.Id);
+                updatestatement = updatestatement.Set("filename", movie.filename);
+                updatestatement = updatestatement.Set("filedate", movie.filedate);
+                updatestatement = updatestatement.Set("filetype", movie.filetype);
+                updatestatement = updatestatement.Set("filecontents", movie.filecontents);
+                updatestatement = updatestatement.Set("filecontentsparsed", movie.filecontentsparsed);
+                //fetch the details from CustomerDB based on id and pass into view  
+                var result = mongoDatabase.GetCollection<Movie>("moviescripts").UpdateOne(filter, updatestatement);
+                if (result.IsAcknowledged == false)
+                {
+                    return BadRequest("Unable to update Customer  " + movie.filename);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult About()
@@ -29,12 +189,6 @@ namespace ScriptIndexerCore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ ScriptIndexerCore/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ScriptIndexerCore.Models
+{
+    public class Movie
+    {
+        [BsonId] public ObjectId Id { get; set; }
+
+        [BsonElement] [MaxLength(250)]public string filename { get; set; }
+
+        [BsonElement] public DateTime filedate { get; set; }
+
+        [BsonElement] public string filetype { get; set; }
+
+        [BsonElement] public string filecontents { get; set; }
+
+        [BsonElement] public string filecontentsparsed { get; set; }
+
+        public string IdStripped
+        {
+            get
+            {
+                return Id.ToString();
+            }
+            
+        }
+
+    }
+}
+
